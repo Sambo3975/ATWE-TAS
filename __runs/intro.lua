b0cdf7d03410eb15840cc8eefd32e7a2ac54188e
Sambo3975 <samboknight@gmail.com>
2021-12-02 12:31:15 -0800

    Restructure

--- __runs/intro.lua
+++
@@ -1,4 +0,0 @@
-return {
-	global = true,
-	"ai",{100}, -- enable the secret speedrun mode
-}
--- luna.lua
+++
@@ -1,6 +0,0 @@
--- small mod for TAS
-if Misc.saveSlot() == 2 or Misc.inEditor() then
-	require("__speedrun/speedrun")
-end
-
-require("globalStuff")
